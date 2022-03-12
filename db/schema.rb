237e85987f6d8f8f4aa7c34b06f33cd2b63c6f4b
pamapama1 <pamapama125@gmail.com>
2022-03-12 11:01:47 +0900

    エラーメッセージ機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_143144) do
+ActiveRecord::Schema.define(version: 2022_03_11_145609) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
