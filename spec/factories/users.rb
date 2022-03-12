44a21f6cca236f65ee659a0f5d8b8fa5d46315a1
pamapama1 <pamapama125@gmail.com>
2022-03-12 19:14:47 +0900

    フッカー導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              {'test'}
-    email                 {'test@example'}
+    email                 {Faker::Internet.free_email}
     password              {'aaaaaa1'}
     password_confirmation {password}
     first_name            {'テスト'}
