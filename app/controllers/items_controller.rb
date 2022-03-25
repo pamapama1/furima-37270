aab9aa95bab3a8d8014e59953cd7ac70e89ea28f
pamapama1 <pamapama125@gmail.com>
2022-03-25 23:25:10 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,21 +21,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to controller: :items, action: :show
     else
       render 'items/edit'
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
