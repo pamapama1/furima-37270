b89cba7f0d4eb443df26737cb72deff7b118b1a7
pamapama1 <pamapama125@gmail.com>
2022-03-25 18:29:09 +0900

    商品編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
+
   root to: 'items#index'
 
   #resources :users, only:[:create]
-  resources :items, only:[:new , :create ,:index, :show]
+  resources :items, only:[:new , :create ,:index, :show, :edit, :update]
 end
