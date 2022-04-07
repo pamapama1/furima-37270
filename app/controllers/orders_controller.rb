6f9a1118e8ef49fc44fe85a8e1174eee2f8754fa
pamapama1 <pamapama125@gmail.com>
2022-04-07 19:08:29 +0900

    修正５

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,6 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
